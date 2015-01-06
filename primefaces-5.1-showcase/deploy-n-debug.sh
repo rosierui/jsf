c7d253fd6baebd903c0b70d889bb26f306f12874
Jonathan Luo <jonathan.luo@zoho.com>
2015-01-06 15:10:50 -0700

    updated tomcat scripts

--- primefaces-5.1-showcase/deploy-n-debug.sh
+++ primefaces-5.1-showcase/deploy-n-debug.sh
@@ -1,5 +1,5 @@
-rm /g01/srv/tomcat-7.0.54/webapps/showcase-5.1.war
-rm -rf /g01/srv/tomcat-7.0.54/webapps/showcase-5.1
+WORKDIR=`pwd`
+TOMCAT=/g01/srv/tomcat-8.0.11
 
 TASK_RUNNING=`ps -eaf | grep "sync-source" | grep -v grep | wc -l`
 if [ $TASK_RUNNING -gt 0 ]; then
@@ -18,26 +18,29 @@ if [ $TASK_RUNNING -gt 0 ]; then
     echo ""
     echo "tomcat is running, shutdown..."
     echo ""
-    /g01/srv/tomcat-7.0.54/bin/shutdown.sh
+    $TOMCAT/bin/shutdown.sh
 else
     echo ""
     echo "tomcat is not running..."
     echo ""
 fi;
 
+rm $TOMCAT/webapps/showcase-5.1.war
+rm -rf $TOMCAT/webapps/showcase-5.1
+
 
 echo "rebuild and deploy..."
 echo
 mvn package
-cp target/showcase-5.1-SNAPSHOT.war /g01/srv/tomcat-7.0.54/webapps/showcase-5.1.war
+cp target/showcase-5.1-SNAPSHOT.war $TOMCAT/webapps/showcase-5.1.war
 
 echo ""
 echo "starting up tomcat..."
 echo ""
-cd /g01/srv/tomcat-7.0.54/bin
+cd $TOMCAT/bin
 ./debug.sh
 
-cd ~/workspace/primefaces-5.1/showcase
+cd $WORKDIR
 
 echo ""
 echo "starting up tomcat source sync"
