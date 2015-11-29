ffe55e78fdf94c262a608764753232527aa38ccd
jonathanluo <jonathan.x.luo@gmail.com>
2015-11-28 17:24:34 -0700

    login page update

--- flagshiptut/src/main/java/org/moonwave/view/UserLoginView.java
+++ flagshiptut/src/main/java/org/moonwave/view/UserLoginView.java
@@ -11,12 +11,10 @@ import org.moonwave.jpa.model.User;
 import org.moonwave.util.SHAUtil;
 
 @ManagedBean
-//@SessionScoped
 public class UserLoginView extends BaseView {
-     
+
     private String username;
     private String password;
-	boolean signIn = false;
  
     public String getUsername() {
         return username;
@@ -35,56 +33,33 @@ public class UserLoginView extends BaseView {
     }
 
     public Boolean isUserLogggedIn() {
-//    	Boolean ret = false;
         Object ret = FacesContext.getCurrentInstance().getExternalContext().getSessionMap().get("isUserLoggedIn");
+        Object user = FacesContext.getCurrentInstance().getExternalContext().getSessionMap().get("loggedInUser");
         return (ret != null) ? true : false; 
     }
 
     public String login() {
-    	
-    	
-//        RequestContext context = RequestContext.getCurrentInstance();
-//        FacesMessage message = null;
-//        boolean loggedIn = false;
-//
-//        if(username != null && username.equals("admin") && password != null && password.equals("admin")) {
-//            loggedIn = true;
-//            message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Welcome", username);
-//
-//            ExternalContext ec = FacesContext.getCurrentInstance().getExternalContext();
-//            HttpSession session = (HttpSession) (ec.getSession(false));
-//            //Map o = (Map) FacesContext.getCurrentInstance().getExternalContext().getSessionMap().get("userProfile");
-//            FacesContext.getCurrentInstance().getExternalContext().getSessionMap().put("isUserLoggedIn", Boolean.TRUE);
-//        } else {
-//            loggedIn = false;
-//            message = new FacesMessage(FacesMessage.SEVERITY_WARN, "Loggin Error", "Invalid credentials");
-//        }
-//         
-//        FacesContext.getCurrentInstance().addMessage(null, message);
-//        context.addCallbackParam("loggedIn", loggedIn);
-//    	========================================================================
-//        return "index.xhtml?redirect=true";//ok
-//        return "index.xhtml?faces-redirect=true";//ok
-//        return "index?faces-redirect=true";//ok
-//        return "/admin/newuser.xhtml?faces-redirect=true";//ok
-    	
         EntityManager em = super.getBasebo().getEntityManager();
-
-        // TutorGroup
         Query query = em.createQuery("Select u from User u where u.loginId = ?1 and u.password = ?2", User.class);
         query.setParameter(1, username);
         query.setParameter(2, SHAUtil.encryptPassword(password));
         List<User> list = query.getResultList();
         User user;
-    	if (list.isEmpty()) {
-    		super.error("Invalid credentials");
-    		return "";
-    	}
-    	// login successful
-		user = list.get(0);
+        if (list.isEmpty()) {
+            super.error("Invalid credentials");
+            return "";
+        }
+        // login successful
+        user = list.get(0);
+        User loggedInUser = new User();
+        loggedInUser.setFirstName(user.getFirstName());
+        loggedInUser.setLastName(user.getLastName());
+        loggedInUser.setLoginId(user.getLoginId());
+        loggedInUser.setTimezone(user.getTimezone());
+        loggedInUser.setEmail(user.getEmail());
+        loggedInUser.setPhone(user.getPhone());
         FacesContext.getCurrentInstance().getExternalContext().getSessionMap().put("isUserLoggedIn", Boolean.TRUE);
-        FacesContext.getCurrentInstance().getExternalContext().getSessionMap().put("loggedInUser", user);
-        return "/admin/newuser?faces-redirect=true";//ok
-//        return "admin/newuser?faces-redirect=true";//ok
+        FacesContext.getCurrentInstance().getExternalContext().getSessionMap().put("loggedInUser", loggedInUser);
+        return "/admin/newuser?faces-redirect=true";
     }
 }
