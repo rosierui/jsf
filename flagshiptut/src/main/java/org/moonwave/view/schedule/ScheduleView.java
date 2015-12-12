380924c954cbff5e8dc73cc771fe9c22bea416cd
jonathanluo <jonathan.x.luo@gmail.com>
2015-12-11 20:14:26 -0700

    schedule load data from db

--- flagshiptut/src/main/java/org/moonwave/view/schedule/ScheduleView.java
+++ flagshiptut/src/main/java/org/moonwave/view/schedule/ScheduleView.java
@@ -11,6 +11,7 @@ import javax.faces.bean.ViewScoped;
 import javax.faces.context.FacesContext;
 import javax.faces.event.ActionEvent;
 
+import org.moonwave.jpa.bo.ScheduleBO;
 import org.moonwave.jpa.bo.UserBO;
 import org.moonwave.jpa.model.Schedule;
 import org.moonwave.jpa.model.User;
@@ -21,7 +22,6 @@ import org.primefaces.event.ScheduleEntryResizeEvent;
 import org.primefaces.event.SelectEvent;
 import org.primefaces.model.DefaultScheduleEvent;
 import org.primefaces.model.DefaultScheduleModel;
-import org.primefaces.model.LazyScheduleModel;
 import org.primefaces.model.ScheduleEvent;
 import org.primefaces.model.ScheduleModel;
 import org.slf4j.Logger;
@@ -35,9 +35,6 @@ public class ScheduleView extends BaseView {
     static final Logger LOG = LoggerFactory.getLogger(ScheduleView.class);
 
     private ScheduleModel eventModel;
-
-    private ScheduleModel lazyEventModel;
-
     private ScheduleEvent event = new DefaultScheduleEvent();
 
     private List<User> students;
@@ -51,22 +48,16 @@ public class ScheduleView extends BaseView {
         students = new UserBO().findAllStudents();
 
         eventModel = new DefaultScheduleModel();
-        eventModel.addEvent(new DefaultScheduleEvent("Champions - today", previousDay8Pm(), previousDay11Pm(), "fc-foday"));
-        eventModel.addEvent(new DefaultScheduleEvent("Birthday - filled", today1Pm(), today6Pm(), "filled"));
-        eventModel.addEvent(new DefaultScheduleEvent("Breakfast - available", nextDay9Am(), nextDay11Am(), "available"));
-        eventModel.addEvent(new DefaultScheduleEvent("Plant the new garden stuff", theDayAfter3Pm(), fourDaysLater3pm()));
+        
+        List<Schedule> schedules = new ScheduleBO().getAllRoles();
+        for (Schedule s : schedules) {
+            eventModel.addEvent(scheduleToEvent(s));
+        }
+//        eventModel.addEvent(new DefaultScheduleEvent("Champions - today", previousDay8Pm(), previousDay11Pm(), "fc-foday"));
+//        eventModel.addEvent(new DefaultScheduleEvent("Birthday - filled", today1Pm(), today6Pm(), "filled"));
+//        eventModel.addEvent(new DefaultScheduleEvent("Breakfast - available", nextDay9Am(), nextDay11Am(), "available"));
+//        eventModel.addEvent(new DefaultScheduleEvent("Plant the new garden stuff", theDayAfter3Pm(), fourDaysLater3pm()));
 
-        lazyEventModel = new LazyScheduleModel() {
-
-            @Override
-            public void loadEvents(Date start, Date end) {
-                Date random = getRandomDate(start);
-                addEvent(new DefaultScheduleEvent("Lazy Event 1", random, random));
-                
-                random = getRandomDate(start);
-                addEvent(new DefaultScheduleEvent("Lazy Event 2", random, random));
-            }
-        };
     }
 
     public String getTutorId() {
@@ -118,81 +109,6 @@ public class ScheduleView extends BaseView {
         return eventModel;
     }
 
-    public ScheduleModel getLazyEventModel() {
-        return lazyEventModel;
-    }
-
-    private Calendar today() {
-        Calendar calendar = Calendar.getInstance();
-        calendar.set(calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DATE), 0, 0, 0);
-
-        return calendar;
-    }
-
-    private Date previousDay8Pm() {
-        Calendar t = (Calendar) today().clone();
-        t.set(Calendar.AM_PM, Calendar.PM);
-        t.set(Calendar.DATE, t.get(Calendar.DATE) - 1);
-        t.set(Calendar.HOUR, 8);
-        return t.getTime();
-    }
-
-    private Date previousDay11Pm() {
-        Calendar t = (Calendar) today().clone();
-        t.set(Calendar.AM_PM, Calendar.PM);
-        t.set(Calendar.DATE, t.get(Calendar.DATE) - 1);
-        t.set(Calendar.HOUR, 11);
-        return t.getTime();
-    }
-
-    private Date today1Pm() {
-        Calendar t = (Calendar) today().clone();
-        t.set(Calendar.AM_PM, Calendar.PM);
-        t.set(Calendar.HOUR, 1);
-        
-        return t.getTime();
-    }
-
-    private Date theDayAfter3Pm() {
-        Calendar t = (Calendar) today().clone();
-        t.set(Calendar.DATE, t.get(Calendar.DATE) + 2);
-        t.set(Calendar.AM_PM, Calendar.PM);
-        t.set(Calendar.HOUR, 3);
-        return t.getTime();
-    }
-
-    private Date today6Pm() {
-        Calendar t = (Calendar) today().clone(); 
-        t.set(Calendar.AM_PM, Calendar.PM);
-        t.set(Calendar.HOUR, 6);
-        return t.getTime();
-    }
-
-    private Date nextDay9Am() {
-        Calendar t = (Calendar) today().clone();
-        t.set(Calendar.AM_PM, Calendar.AM);
-        t.set(Calendar.DATE, t.get(Calendar.DATE) + 1);
-        t.set(Calendar.HOUR, 9);
-
-        return t.getTime();
-    }
-
-    private Date nextDay11Am() {
-        Calendar t = (Calendar) today().clone();
-        t.set(Calendar.AM_PM, Calendar.AM);
-        t.set(Calendar.DATE, t.get(Calendar.DATE) + 1);
-        t.set(Calendar.HOUR, 11);
-        return t.getTime();
-    }
-
-    private Date fourDaysLater3pm() {
-        Calendar t = (Calendar) today().clone(); 
-        t.set(Calendar.AM_PM, Calendar.PM);
-        t.set(Calendar.DATE, t.get(Calendar.DATE) + 4);
-        t.set(Calendar.HOUR, 3);
-        return t.getTime();
-    }
-
     public ScheduleEvent getEvent() {
         return event;
     }
@@ -237,8 +153,6 @@ public class ScheduleView extends BaseView {
 
     public void onViewChange(SelectEvent selectEvent) {
         Object obj = selectEvent.getObject();
-        Object tt = obj;
-        String viewName = selectEvent.getObject().toString();
     }
 
     public void onTodaySelect(SelectEvent selectEvent) {
@@ -282,9 +196,20 @@ public class ScheduleView extends BaseView {
         s.setUserId(Integer.parseInt(studentId));
         s.setEvent(event.getTitle());
         s.setAllDayEvent(event.isAllDay());
-        s.setStartTime(new java.sql.Timestamp(event.getStartDate().getTime()));
-        s.setEndTime(new java.sql.Timestamp(event.getEndDate().getTime()));
+        s.setStartTime(event.getStartDate());
+        s.setEndTime(event.getEndDate());
         s.setCreateTime(super.getSqlTimestamp());
         return s;
+    }
+
+    private ScheduleEvent scheduleToEvent(Schedule s) {
+        DefaultScheduleEvent e = new DefaultScheduleEvent();
+        e.setAllDay(s.getAllDayEvent());
+        e.setEditable(false);
+        e.setTitle(s.getEvent());
+        e.setStartDate(s.getStartTime());
+        e.setEndDate(s.getEndTime());
+        e.setStyleClass("filled");
+        return e;
     }
 }
