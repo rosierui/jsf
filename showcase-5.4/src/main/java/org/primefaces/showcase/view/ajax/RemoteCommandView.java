38c75d6d57152f991db1fdba650f4b990149ac3f
moonwave <moonwave.systems@gmail.com>
2016-03-15 11:41:32 -0700

    remoteCommand with parameters

--- showcase-5.4/src/main/java/org/primefaces/showcase/view/ajax/RemoteCommandView.java
+++ showcase-5.4/src/main/java/org/primefaces/showcase/view/ajax/RemoteCommandView.java
@@ -17,12 +17,49 @@ package org.primefaces.showcase.view.ajax;
 
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
 import javax.faces.context.FacesContext;
 
+/**
+ * http://stackoverflow.com/questions/7221495/pass-parameter-to-premotecommand-from-javascript
+ * 
+ */
 @ManagedBean
 public class RemoteCommandView {
 
     public void execute() {
         FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Executed", "Using RemoteCommand."));
     }
+
+    // Test URL: http://localhost/showcase-5.4/ui/ajax/remoteCommandJ.xhtml
+    // Pass parameter to p:remoteCommand from JavaScript
+    // added the following
+
+    public void execute2() {
+        FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Executed", 
+            "Using RemoteCommand with parameter x := " + x + ", y := " + y));
+    }
+
+    @ManagedProperty("#{param.x}")
+    private int x;
+
+    @ManagedProperty("#{param.y}")
+    private int y;
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
 }
